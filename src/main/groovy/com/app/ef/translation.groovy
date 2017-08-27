4513e8ab17eaa3f2179b066e1e3897cdfaf9e704
Saqib Ahmed <saqibahmed515@gmail.com>
2017-08-27 23:37:35 +0500

    Refactored some code to add the corner cases.

--- src/main/groovy/com/app/ef/translation.groovy
+++ src/main/groovy/com/app/ef/translation.groovy
@@ -5,33 +5,52 @@ package com.app.ef
 
 import groovy.json.JsonSlurper
 import groovyx.net.http.HTTPBuilder
+import groovy.json.JsonBuilder
 
 class translation {
+    static def x=1
     public static void main(String[] args){
+        def obj = new translation()
         def dir = args[0];
         def tl = args[1];
-        def http = new HTTPBuilder('https://translate.googleapis.com')
-//        def html = http.post(path : '/translate_a/single',
-//                query : [client:"gtx", sl :"auto", tl: tl , dt:"t", q: "lorem ipsum sit dolor amet"])
-//        println html[0][0][0]
+        def outFile = new File("${tl}.json")
+        if(outFile.exists())
+          outFile.delete()
+
         def inputFile = new File(dir)
-        def inputJSON = new JsonSlurper().parse(inputFile)
-//        println inputJSON
-        inputJSON.each{k, v->
-//            println "${k}:${v}"
-            v.each{k1, v1->
-                println "${k1}:${v1}"
-                if(v1.class){
-                    def html = http.post(path : '/translate_a/single',
-                            query : [client:"gtx", sl :"auto", tl: tl , dt:"t", q: v1])
-                    println html[0][0][0]
-                }
-            }
-//            it.each{k, v ->
-//                html = http.post(path : '/translate_a/single',
-//                        query : [client:"gtx", sl :"auto", tl: tl , dt:"t", q: v])
-//                println html[0][0][0]
-//            }
+        inputFile.eachLine { line ->
+          if(line.endsWith(',') || line.endsWith('\"')){
+            def tok = line.tokenize(':')
+            println tok[1]
+            def out = obj.translate(tok[1], tl)
+            println out
+            def outLine = line.replaceAll(tok[1], out)
+            println outLine
+            outFile.append("\n"+outLine)
+          } else{
+            outFile.append("\n" + line)
+          }
         }
+    }
+
+    def translate(def str, def tl){
+      def http = new HTTPBuilder('https://translate.googleapis.com')
+      def html
+      try{
+        if(x==1){
+          println "Getting translation"
+          html = http.post(path : '/translate_a/single',
+                  query : [client:"gtx", sl :"auto", tl: tl , dt:"t", q: str])
+          println html[0][0][0]
+          return html[0][0][0]
+        }
+        else{
+          throw new Exception()
+        }
+      }catch(Exception ex){
+        x++
+        str = str.replaceFirst('\"' , '\"${tl}_')
+        return str + "_" + tl
+      }
     }
 }
